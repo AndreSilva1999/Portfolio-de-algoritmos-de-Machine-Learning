2417afe88245faefedaa4bd2516b28d56160d80b
André Silva <90265035+AndreSilva1999@users.noreply.github.com>
2022-10-11 13:37:40 +0100

    Add files via upload

---
+++ src/si/SelectPercentile.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import sys
+sys.path.append("/Users/André Silva/SI/")
+from dataset import Dataset
+from si.f_classification import f_classification
+
+class selecetPercentile:
+    
+    def __init__(self,dataset,p) -> None:
+        self.dataset=dataset
+        self.score_func= f_classification(dataset)
+        self.percentile= int(p*len(dataset.features))#Acrecentar o  tamanho do dataset
+        self.F=None
+        self.p=None
+    
+    def fit(self):
+        self.F, self.p= self.score_func
+        return self
+
+    def transform(self):
+        indxs= np.argsort(self.F)[-self.percentile]
+        features= np.array(self.dataset.features)[indxs]
+        return Dataset(self.dataset.x[:, indxs], self.dataset.y,list(features), self.dataset.label)
+
+    def fit_transform(self):
+        self.fit()
+        return self.transform()
+
+
+
+if __name__== "__main__":
+    from si.read_csv import read_csv
+    data= read_csv("iris.csv",sep=",")
+    percent= selecetPercentile(data,0.50)
+    # print(data.get_classes())
+    percent.fit_transform()
+    # x= np.array([[1,2,3],[1,2,3]])
+    # y= np.array([1,2])
+    # features=["A","B","C"]
+    # label= "y"
+    # dataset= Dataset(x=x,y=y,features=features,label=label)
+    # percent= selecetPercentile(dataset,0.50)
+    # print(percent.fit_transform())
---
+++ src/si/f_classification.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import sys
+sys.path.append("/Users/André Silva/SI/")
+from dataset import Dataset
+
+
+def f_classification(dataset:Dataset):
+    classes = dataset.get_classes()
+    groups = [dataset.x[dataset.y == c] for c in classes]
+    F, p = stats.f_oneway(*groups)
+    print(F,p)
+    return F, p
