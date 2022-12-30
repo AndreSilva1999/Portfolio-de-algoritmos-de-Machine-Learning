0ab336e1cd763d5314c6d48357ce145ade5e593e
André Silva <90265035+AndreSilva1999@users.noreply.github.com>
2022-12-30 14:22:31 +0000

    Add files via upload

---
+++ src/si/feature_selection/SelectPercentile.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+import sys
+sys.path.append("/Users/André Silva/SI/")
+from si.data.dataset1 import Dataset
+from si.statistics.f_classification import f_classification
+
+class selecetPercentile:
+    
+    def __init__(self,dataset,p,scoring_function) -> None:
+        """_summary_
+        Initialize parameters
+        Args:   
+            dataset (Dataset): Dataset
+            p (float): percentage
+        """
+        self.dataset=dataset
+        self.score_func= scoring_function(dataset)
+        self.percentile= int(p*len(dataset.features))#Acrecentar o  tamanho do dataset (Eu quero p*features = 0.50*20=10)
+        self.F=None
+        self.p=None
+    
+    def fit(self):
+        """_summary_
+            Does fit
+        Returns :
+            Self
+        """
+        self.F, self.p= self.score_func
+        return self
+
+    def transform(self):
+        """_summary_
+        Select k best features
+        Args:
+            dataset (Dataset): Dataset
+
+        Returns:
+            Dataset: 
+        """
+        indxs= np.argsort(self.F)[-self.percentile]
+        features= np.array(self.dataset.features)[indxs]
+        return Dataset(self.dataset.X[:, indxs], self.dataset.y,list(features), self.dataset.label)
+
+    def fit_transform(self):
+        """_summary_
+        Does fit and transform 
+        Args:
+            dataset (_type_): Dataset
+
+        Returns:
+            _type_: Returns full dataset
+        """
+        self.fit()
+        return self.transform()
+
+
+
+if __name__== "__main__":
+    from si.read_csv import read_csv
+    data= read_csv("C:/Users/André Silva/SI/datasets/iris.csv",sep=",")
+    percent= selecetPercentile(data,0.50,f_classification)
+    # print(data.get_classes())
+    datasetnew=percent.fit_transform()
+    print(datasetnew.X)
+    
+    # x= np.array([[1,2,3],[1,2,3]])
+    # y= np.array([1,2])
+    # features=["A","B","C"]
+    # label= "y"
+    # dataset= Dataset(x=x,y=y,features=features,label=label)
+    # percent= selecetPercentile(dataset,0.50)
+    # print(percent.fit_transform())
